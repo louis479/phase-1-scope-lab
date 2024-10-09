f292b8a462f248d9d196eb46b96155fedfe87896
louis-ngugi <louisngugi858@gmail.com>
2024-10-09 12:53:40 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+
+// Declare customerName in global scope
+window.customerName = 'bob';
+
+// Function to modify customerName to uppercase
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+// Declare bestCustomer in global scope
+window.bestCustomer = undefined;
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(name) {
+  window.bestCustomer = name;
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'initial value';
+
+// Function that tries to change leastFavoriteCustomer 
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value';
+}
